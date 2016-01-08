1d586db0959c3951373f2107ddf5fedd6cb86101
Trung Hậu <trunghau.t1@gmail.com>
2016-01-08 14:43:44 +0700

    Update Entity

--- Quiz/Model/QuizModel.Designer.cs
+++ Quiz/Model/QuizModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\Hoc tap\GitHub\QuizApplication\Quiz\Model\QuizModel.edmx'. 
+﻿// T4 code generation is enabled for model 'E:\Documents\Visual Studio 2013\Projects\Quiz\Quiz\Model\QuizModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
